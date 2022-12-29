505636c94d9ecb7e543dbe5599ac02d19e5991db
Chloe <chloe@chloeveraloo.com>
2022-12-29 14:19:45 -0800

    Test coverage regression

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ['@babel/preset-env', { targets: { node: 'current' }, modules: false }],
+      ['@babel/preset-env', { modules: false, targets: { node: 'current' } }],
       '@babel/preset-typescript',
     ],
   };
